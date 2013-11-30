4ce8464848fddc86b8cede7fbeac290f5bb17ecc
Henrique Bastos <henrique@bastos.net>
2013-11-30 18:10:55 -0200

    Add SubscribeTest.test_get

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -1,3 +1,13 @@
+# coding: utf-8
 from django.test import TestCase
 
-# Create your tests here.
+
+class SubscribeTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/inscricao/')
+
+    def test_get(self):
+        """
+        Get /inscricao/ must return status code 200.
+        """
+        self.assertEqual(200, self.resp.status_code)
