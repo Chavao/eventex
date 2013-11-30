b4b1883d50b0b43376456674d3f07cefbf7909ab
Henrique Bastos <henrique@bastos.net>
2013-11-30 17:34:32 -0200

    One assert per test method

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -3,10 +3,17 @@ from django.test import TestCase
 
 
 class HomeTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/')
+
     def test_get(self):
         """
         GET / must return status code 200.
         """
-        response = self.client.get('/')
-        self.assertEqual(200, response.status_code)
-        self.assertTemplateUsed(response, 'index.html')
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        """
+        Home must use template index.html
+        """
+        self.assertTemplateUsed(self.resp, 'index.html')
