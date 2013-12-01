10dd7740bb8cf2a925ab3d93bc1c0a906e728143
Henrique Bastos <henrique@bastos.net>
2013-12-01 01:54:40 -0200

    Save on valid POST

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -2,11 +2,16 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
 
 
 def subscribe(request):
     if request.method == 'POST':
-        return HttpResponseRedirect('/inscricao/%d/' % 1)
+        form = SubscriptionForm(request.POST)
+        form.full_clean()
+        obj = Subscription(**form.cleaned_data)
+        obj.save()
+        return HttpResponseRedirect('/inscricao/%d/' % obj.pk)
     else:
         return render(request,
                       'subscriptions/subscription_form.html',
