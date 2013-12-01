cd810bf3ac23c72e6e80675860a90b27623e87f7
Henrique Bastos <henrique@bastos.net>
2013-12-01 02:11:35 -0200

    Refactor to separate subscribe's responsabilities

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -6,15 +6,21 @@ from eventex.subscriptions.forms import SubscriptionForm
 
 def subscribe(request):
     if request.method == 'POST':
-        form = SubscriptionForm(request.POST)
-        if form.is_valid():
-            obj = form.save()
-            return HttpResponseRedirect('/inscricao/%d/' % obj.pk)
-        else:
-            return render(request,
-                          'subscriptions/subscription_form.html',
-                          {'form': form})
+        return create(request)
     else:
-        return render(request,
-                      'subscriptions/subscription_form.html',
-                      {'form': SubscriptionForm()})
+        return new(request)
+
+
+def new(request):
+    return render(request, 'subscriptions/subscription_form.html',
+                  {'form': SubscriptionForm()})
+
+
+def create(request):
+    form = SubscriptionForm(request.POST)
+    if not form.is_valid():
+        return render(request, 'subscriptions/subscription_form.html',
+              {'form': form})
+
+    obj = form.save()
+    return HttpResponseRedirect('/inscricao/%d/' % obj.pk)
