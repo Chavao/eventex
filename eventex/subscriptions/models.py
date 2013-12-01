714ffef9c768a6dde094f0668bf3398f530aeabe
Henrique Bastos <henrique@bastos.net>
2013-12-01 01:29:46 -0200

    Implement model fields

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -3,4 +3,8 @@ from django.db import models
 
 
 class Subscription(models.Model):
-    pass
+    name = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=11)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20, blank=True)
+
