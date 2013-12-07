3109f32184f7c0945cfa9e83b7634aa036ee225d
Henrique Bastos <henrique@bastos.net>
2013-12-07 16:47:54 -0200

    Test name is capitalized

--- eventex/subscriptions/tests/test_forms.py
+++ eventex/subscriptions/tests/test_forms.py
@@ -32,6 +32,13 @@ class SubscriptionFormTest(TestCase):
         form = self.make_validated_form(email='')
         self.assertFalse(form.errors)
 
+    def test_name_must_br_capitalized(self):
+        """
+        Name must be capitalized.
+        """
+        form = self.make_validated_form(name='HENRIQUE bastos')
+        self.assertEqual('Henrique Bastos', form.cleaned_data['name'])
+
     def make_validated_form(self, **kwargs):
         data = dict(name='Henrique Bastos', email='henrique@bastos.net',
                     cpf='12345678901', phone='21-996186180')
