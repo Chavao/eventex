0bca3959b589bb979a41a71e5ad155e43d512cb2
Henrique Bastos <henrique@bastos.net>
2013-12-07 17:27:46 -0200

    Refactor html test

--- eventex/subscriptions/tests/test_views_subscribe.py
+++ eventex/subscriptions/tests/test_views_subscribe.py
@@ -26,8 +26,8 @@ class SubscribeTest(TestCase):
         Html must contain input controls.
         """
         self.assertContains(self.resp, '<form')
-        self.assertContains(self.resp, '<input', 6)
-        self.assertContains(self.resp, 'type="text"', 3)
+        self.assertContains(self.resp, '<input', 7)
+        self.assertContains(self.resp, 'type="text"', 4)
         self.assertContains(self.resp, 'type="email"')
         self.assertContains(self.resp, 'type="submit"')
 
