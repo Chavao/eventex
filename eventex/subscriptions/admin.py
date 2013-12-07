5c52abea41144e04fee7bf7e5e5b7cf887faed04
Henrique Bastos <henrique@bastos.net>
2013-12-07 14:49:02 -0200

    Add subscribed_today

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -1,12 +1,16 @@
 # coding: utf-8
 from django.contrib import admin
+from django.utils.datetime_safe import datetime
 from eventex.subscriptions.models import Subscription
 
 
 class SubscriptionAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'cpf', 'phone', 'created_at')
+    list_display = ('name', 'email', 'cpf', 'phone', 'created_at', 'subscribed_today')
     date_hierarchy = 'created_at'
     search_fields = ('name', 'email', 'cpf', 'phone', 'created_at')
+
+    def subscribed_today(self, obj):
+        return obj.created_at.date() == datetime.today().date()
 
 
 admin.site.register(Subscription, SubscriptionAdmin)
