d619c6cf3047711abcb4e794b451ca6390beb2d0
Henrique Bastos <henrique@bastos.net>
2013-12-07 12:53:26 -0200

    Extract core urls to the app's urlconf.

--- eventex/urls.py
+++ eventex/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'eventex.core.views.home', name='home'),
+    url(r'', include('eventex.core.urls', namespace='core')),
     url(r'^inscricao/$', 'eventex.subscriptions.views.subscribe', name='subscribe'),
     url(r'^inscricao/(\d+)/$', 'eventex.subscriptions.views.detail', name='detail'),
     url(r'^admin/', include(admin.site.urls)),
