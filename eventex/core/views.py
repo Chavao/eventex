55d313c8f21d3cea90d782c69a9fe66c600ec00f
Henrique Bastos <henrique@bastos.net>
2013-12-14 16:55:22 -0200

    Implement speaker_detail as a dummy view.

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,6 +1,12 @@
 # coding: utf-8
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def home(request):
     return render(request, 'index.html')
+
+
+def speaker_detail(request, slug):
+    return HttpResponse()
+
